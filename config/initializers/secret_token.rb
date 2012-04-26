cb901af3abb24b33bd92718c0f779ff03b203147
Marcin Kulik <marcin.kulik@gmail.com>
2012-04-26 13:17:06 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luna::Application.config.secret_token = '3898a0da2b6f5a135cc9efc4b452a760b45c67db3114d641a2d7769a5a1d632649b74b010f1238627f69871f7fbc3ddcf0f75401797346b3f0be940ff501e520'
